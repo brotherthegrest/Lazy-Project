b1003cce684bffddbde2105032f33c24965d7081
alantheitelligent <alanhild715@gmail.com>
2024-01-18 14:02:39 +0000

    restarting

--- README.md
+++
@@ -1,2 +0,0 @@
-# Lazy-Project
-terminal based project manager 
--- cmd/root.go
+++ cmd/root.go
@@ -1,80 +1,34 @@
 /*
-Copyright © 2023 Alan Hildebrandt <Alanhild715@gmail.com>
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
 import (
-	"fmt"
-
 	"os"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	"github.com/charmbracelet/bubbles/viewport"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
-// calling the full screen msg
-type startupcalls struct{}
 
-type model struct {
-	left       viewport.Model
-	right      viewport.Model
-	input      textinput.Model
-	width      int
-	height     int
-	fullscreen bool
-}
 
-func (m model) Init() tea.Cmd {
-	return nil
-
-}
-
-// dosent matter
-func initialModel() model {
-	return model{}
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case tea.KeyMsg:
-
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		}
-
-		m.left, _ = m.left.Update(msg)
-		m.right, _ = m.right.Update(msg)
-		m.input, _ = m.input.Update(msg)
-
-	}
-	return m, nil
-
-}
-
-func (m model) View() string {
-	return m.left.View() + m.input.View() + m.right.View()
-
-}
-
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "Lazy-Project",
-	Short: "terminal based project manager",
-	Long:  `Lazy-Project is a terminal based project manager insipred by LazyGit`,
-	Run: func(cmd *cobra.Command, args []string) {
-		m := model{}
-		p := tea.NewProgram(&m)
-		if _, err := p.Run(); err != nil {
-			fmt.Printf("could not start program: %v", err)
-			os.Exit(1)
-		}
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
 
-	},
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -93,3 +47,5 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
+
